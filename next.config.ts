4937911371f4d5b688e2b3f1abf75a682c2badfd
Sup3rga <wilkysynd@gmail.com>
2026-07-27 23:07:40 -0400

    remove webpack

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
     // Pour les Server Components, exclure mysql2 du bundling
     serverExternalPackages: ['mysql2'],
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            config.externals = [...(config.externals || []), 'mysql2', 'mysql2/promise'];
-        }
-        return config;
-    },
-    experimental: {
-        serverExternalPackages: ['mysql', 'serverless-mysql', 'mysql2', 'mysql2/promise'],
-    },
     eslint: {
         ignoreDuringBuilds: true,
     },

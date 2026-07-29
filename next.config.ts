212a36b29a9de5d54c8c6739da9cdc31d8b07f3a
Sup3rga <wilkysynd@gmail.com>
2026-07-29 13:14:25 -0400

    adding header for _next

--- next.config.ts
+++ next.config.ts
@@ -14,10 +14,6 @@ const nextConfig: NextConfig = {
                     has:[{type: "host", value: "management.musautop.com"}]
                 },
                 {
-                    source: "/favicon.png",
-                    destination: "/favicon.png",
-                },
-                {
                     source: '/',
                     has: [{ type: 'host', value: 'management.musautop.com' }],
                     destination: '/cmgr',
@@ -29,6 +25,20 @@ const nextConfig: NextConfig = {
                 },
             ]
         }
+    },
+
+    async headers() {
+        return [
+            {
+                source: "/_next/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
     },
     eslint: {
         ignoreDuringBuilds: true,

bdb5be59ae83f9221e20e1ed6b8febc0bb89bacf
Sup3rga <wilkysynd@gmail.com>
2026-07-31 15:42:01 -0400

    ready for production 99.971%

--- server/images/index.js
+++ server/images/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use('/assets', express.static(path.join(__dirname, '../public/assets')));
+app.use('/assets', express.static(path.join(__dirname, '../../public/assets')));
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
